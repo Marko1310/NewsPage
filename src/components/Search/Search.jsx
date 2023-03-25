800060f00165363e03ce194e712092bcfcdd381f
Marko1310 <marko.cabo13@gmail.com>
2023-03-26 00:26:43 +0100

    search bar completed

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -4,13 +4,15 @@ import searchIcon from "../../assets/Search.png";
 const Search = () => {
   return (
     <div className="title-container">
-      <p className="title">MyNews</p>
+      <p className="title">
+        <span className="title-first">My</span>News
+      </p>
       <div className="search">
         <form className="search-form">
           <input
             className="search-input"
             type="text"
-            placeholder="Search..."
+            placeholder="Search news"
           ></input>
           <button className="search-button" type="submit">
             Search
