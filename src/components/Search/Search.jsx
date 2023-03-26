dd98f093b755cf2a3144e852e374178133c3de07
Marko1310 <marko.cabo13@gmail.com>
2023-03-26 12:13:22 +0200

    main flex container defined

--- src/components/Search/Search.jsx
+++ src/components/Search/Search.jsx
@@ -3,23 +3,23 @@ import searchIcon from "../../assets/Search.png";
 
 const Search = () => {
   return (
-    // <div className="titleSearch-container">
-    //   <p className="title">
-    //     <span>My</span>News
-    //   </p>
-    <div className="search-container">
-      <form className="search-form">
-        <input
-          className="search-input"
-          type="text"
-          placeholder="Search news"
-        ></input>
-        <button className="search-button" type="submit">
-          Search
-        </button>
-      </form>
+    <div className="titleSearch-container">
+      <p className="title">
+        <span>My</span>News
+      </p>
+      <div className="search-container">
+        <form className="search-form">
+          <input
+            className="search-input"
+            type="text"
+            placeholder="Search news"
+          ></input>
+          <button className="search-button" type="submit">
+            Search
+          </button>
+        </form>
+      </div>
     </div>
-    // </div>
   );
 };
 
