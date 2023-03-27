3a283fca895efa87b5a91d71abae7e2eb61aad37
Marko1310 <marko.cabo13@gmail.com>
2023-03-27 08:35:51 +0200

    testing the grid template

--- src/components/News/News.jsx
+++ src/components/News/News.jsx
@@ -4,18 +4,22 @@ import articles from "./articles";
 const News = () => {
   return (
     <div className="news-container">
-      {articles.map((article) => {
-        return (
-          <div className="article-box">
-            <img src={article.imgUrl} />
-            <div className="description-container">
-              <p className="description-group">{article.group}</p>
-              <p className="description-title">{article.title}</p>
-              <p className="description-author">{article.author}</p>
+      <p className="news-title">News</p>
+      <div className="news-gridLayout">
+        {articles.map((article) => {
+          return (
+            <div className="article-box">
+              <img src={article.imgUrl} />
+              <div className="description-container">
+                <p className="description-group">{article.group}</p>
+                <p className="description-title">{article.title}</p>
+                <p className="description-author">{article.author}</p>
+              </div>
             </div>
-          </div>
-        );
-      })}
+          );
+        })}
+        <div className="latestNews"></div>
+      </div>
     </div>
   );
 };
