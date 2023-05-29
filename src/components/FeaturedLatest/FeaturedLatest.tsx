9d5370787deae18c5847071850841e4e16f7d510
Marko1310 <marko.cabo13@gmail.com>
2023-05-29 19:55:34 +0200

    FeaturedLatest component transfered to ts

--- src/components/FeaturedLatest/FeaturedLatest.tsx
+++ src/components/FeaturedLatest/FeaturedLatest.tsx
@@ -1,24 +1,28 @@
-//css
-import "./FeaturedLatest.scss";
+//react
+import React from 'react';
 
-const FeaturedLatest = ({ featuredLatest, toggleFeaturedLatest }) => {
+//css
+import './FeaturedLatest.scss';
+
+interface FeaturedLatestProps {
+  featuredLatest: string;
+  toggleFeaturedLatest: (variable: string) => void;
+}
+
+const FeaturedLatest = ({ featuredLatest, toggleFeaturedLatest }: FeaturedLatestProps) => {
   return (
     <div className="featuredLatest-container">
       <button
         onClick={() => {
-          toggleFeaturedLatest("featured");
+          toggleFeaturedLatest('featured');
         }}
-        className={`featuredLatest-container-button + ${
-          featuredLatest === "featured" ? "featured" : ""
-        }`}
+        className={`featuredLatest-container-button + ${featuredLatest === 'featured' ? 'featured' : ''}`}
       >
         Featured
       </button>
       <button
-        onClick={() => toggleFeaturedLatest("latest")}
-        className={`featuredLatest-container-button + ${
-          featuredLatest === "latest" ? "latest" : ""
-        }`}
+        onClick={() => toggleFeaturedLatest('latest')}
+        className={`featuredLatest-container-button + ${featuredLatest === 'latest' ? 'latest' : ''}`}
       >
         Latest
       </button>
