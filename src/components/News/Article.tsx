2e9d09693e466592360b0eb99cfc9cc39ae71825
Marko1310 <marko.cabo13@gmail.com>
2023-05-30 23:52:43 +0200

    Article to ts

--- src/components/News/Article.tsx
+++ src/components/News/Article.tsx
@@ -1,25 +1,23 @@
 // react
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from 'react';
 
 // css
-import "./Article.scss";
-import "./News.scss";
+import './Article.scss';
+import './News.scss';
 
 // images
-import NoImage from "../../assets/images/No_Image.png";
+// @ts-ignore
+import NoImage from '../../assets/images/No_Image.png';
 
 // fetch calls
-import localStorageServices from "../../services/localStorageServices.js";
+import localStorageServices from '../../services/localStorageServices.js';
 
 const Article = ({ article, category, sources, handleFavorite }) => {
   const [isFavorite, setIsFavorite] = useState(false);
 
   useEffect(() => {
     const storedArticles = localStorageServices.getFavorites();
-    const isStoredArticle = localStorageServices.isFavorite(
-      storedArticles,
-      article
-    );
+    const isStoredArticle = localStorageServices.isFavorite(storedArticles, article);
     if (isStoredArticle) setIsFavorite(true);
     else if (isStoredArticle === undefined) setIsFavorite(false);
   }, [article]);
@@ -27,18 +25,15 @@ const Article = ({ article, category, sources, handleFavorite }) => {
   return (
     <div className="article-box">
       <div className="article-image-container">
-        <img
-          className="article-image"
-          src={!article.urlToImage ? NoImage : article.urlToImage}
-        />
+        <img className="article-image" src={!article.urlToImage ? NoImage : article.urlToImage} />
       </div>
       <div className="article-container">
         <p className="article-category">
-          {category === "Home"
-            ? sources?.find((s) => s.id === article.source.id)?.category
-            : category}
+          {category === 'Home' ? sources?.find((s) => s.id === article.source.id)?.category : category}
         </p>
-        <a className="article-title" href={article.url}>{article.title}</a>
+        <a className="article-title" href={article.url}>
+          {article.title}
+        </a>
         <div className="author-favorite">
           {<p className="article-author">{article.author}</p>}
           <i
@@ -46,7 +41,7 @@ const Article = ({ article, category, sources, handleFavorite }) => {
               setIsFavorite((prevState) => (prevState = !prevState));
               handleFavorite(article);
             }}
-            className={isFavorite ? "fa-solid fa-star" : "fa-regular fa-star"}
+            className={isFavorite ? 'fa-solid fa-star' : 'fa-regular fa-star'}
           ></i>
         </div>
       </div>
