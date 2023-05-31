6b8f1bf79c28e9af9a9e92410c8d3a72eabf8d36
Marko1310 <marko.cabo13@gmail.com>
2023-05-31 11:57:54 +0200

    NewsApiServices to ts finished

--- src/services/newsApiServices.ts
+++ src/services/newsApiServices.ts
@@ -1,26 +1,24 @@
-import axios from "axios";
+import axios from 'axios';
 
 // const API_KEY = "9d082cf8c343429da0f7ccde72fd72e5";
 // const API_KEY = "eef268bd2bf14a57b498ce95b413d433";
 // const API_KEY = "03a53c477965493ab56337906674304e";
-const API_KEY = "bde9b689a4584be0bd5757718405f691";
+const API_KEY = 'bde9b689a4584be0bd5757718405f691';
 // const API_KEY = "f72818b798474a18b18661aea91ec437";
 
 const getSources = function () {
   return axios
-    .get(
-      `https://newsapi.org/v2/top-headlines/sources?country=us&apiKey=${API_KEY}`
-    )
+    .get(`https://newsapi.org/v2/top-headlines/sources?country=us&apiKey=${API_KEY}`)
     .then((data) => {
       return data.data.sources;
     })
     .catch((err) => console.log(err));
 };
 
-const getArticles = function (category, query) {
+const getArticles = function (category: string, query: string) {
   let url = `https://newsapi.org/v2/top-headlines?country=us&pageSize=13&apiKey=${API_KEY}`;
 
-  if (category !== "Home") {
+  if (category !== 'Home') {
     url += `&category=${category}`;
   }
 
@@ -35,11 +33,9 @@ const getArticles = function (category, query) {
     .catch((err) => console.log(err));
 };
 
-const getLatestNews = function (pageSize, page) {
+const getLatestNews = function (pageSize: number, page: number) {
   return axios
-    .get(
-      `https://newsapi.org/v2/everything?domains=bbc.co.uk&pageSize=${pageSize}&page=${page}&apiKey=${API_KEY}`
-    )
+    .get(`https://newsapi.org/v2/everything?domains=bbc.co.uk&pageSize=${pageSize}&page=${page}&apiKey=${API_KEY}`)
     .then((response) => {
       return response.data.articles;
     });
